5e5d00f547d8907d26a817f31ea14a7e5641f2be
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-30 01:39:08 -0300

    Http requests are more generic now with axios. Pics are now shown responsively

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,32 @@ import Busca from './Busca'
 import env from 'react-dotenv' //mantenha
 import { createClient }  from 'pexels'
 import ListaImagens from './ListaImagens'
+import PexelsLogo from './PexelsLogo'
+import pexelsClient from '../utils/pexelsClient'
 
 export default class App extends React.Component {
 
     state = {pics: []}
 
-
+    // onBuscaRealizada = (termo) => {
+    //     this.pexelsClient.photos.search({
+    //         query: termo
+    //     })
+    //     .then(pics => this.setState({pics: pics.photos}))
+    // }
+    // pexelsClient = null
 
     onBuscaRealizada = (termo) => {
-        this.pexelsClient.photos.search({
-            query: termo
+        pexelsClient.get('/search', {
+            params: { query: termo}
         })
-        .then(pics => this.setState({pics: pics.photos}))
+        .then(result => {
+            console.log(result)
+            //data é um atributo definido pela axios
+            //o conteúdo da resposta vem associado a essa chave
+            this.setState ({pics: result.data.photos})
+        })
     }
-
-    pexelsClient = null
 
     componentDidMount(){
         this.pexelsClient = createClient(env.PEXELS_KEY)
@@ -26,13 +37,18 @@ export default class App extends React.Component {
         return (
             <div className="grid justify-content-center m-auto w-9 border-round border-1 border-400">
                 <div className="col-12">
+                    <PexelsLogo/>
+                </div>
+                <div className="col-12">
                     <h1 className="text-center">Exibir uma lista de...</h1>
                 </div>
-                <div className="col-8">
+                <div className="col-12">
                     <Busca onBuscaRealizada={this.onBuscaRealizada}/>
                 </div>
-                <div className="col-8">
-                    <ListaImagens pics={this.state.pics}/>
+                <div className="col-12">
+                    <div className="grid">
+                        <ListaImagens imgStyle={'col-12 md:col-6 lg:col-4 xl:col-3'} pics={this.state.pics}/>
+                    </div>
                 </div>
             </div>
         )
--- src/components/ListaImagens.js
+++ src/components/ListaImagens.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Imagem from './Imagem'
 
-const ListaImagens = ({pics}) => {
+const ListaImagens = ({pics, imgStyle}) => {
+    console.log(pics)
     return (
         pics.map((pic, key) => (
             <Imagem
+                imgStyle={imgStyle}
                 pic={pic.src.small}
                 key={key}
             />

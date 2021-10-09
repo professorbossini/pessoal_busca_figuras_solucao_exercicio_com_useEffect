13eb2da2e06dd8de172f8d0b42949c6318151d40
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-09 12:15:33 -0300

    Solucao exercicio apostila 08 busca de figuras com hook useEffect

--- src/components/Busca.js
+++ src/components/Busca.js
@@ -1,47 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import { InputText } from 'primereact/inputtext'
-import { Button } from 'primereact/button'
-export default class Busca extends Component {
-    state = {
-        termoDeBusca: ''
+//o uso de hooks requer componentes funcionais!!
+const Busca = (props) => {
+    const [termoDeBusca, setTermoDeBusca] = useState('')
+    const atualizarTermoDeBusca = (e) => {
+        setTermoDeBusca(e.target.value)
     }
-    onTermoAlterado = (event) => {
-        console.log(event.target.value)
-        this.setState({termoDeBusca: event.target.value})
-    }
-
-    onFormSubmit = (event) => {
-        //não deixa o navegador submeter o form
-        event.preventDefault()
-        this.props.onBuscaRealizada(this.state.termoDeBusca)
-
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.onFormSubmit}>
-                {/* empilhando os filhos */}
-                <div className="flex flex-column">
-                    {/* ícone à esquerda, largura máxima */}
-                    <span className="p-input-icon-left w-full">
-                        <i className="pi pi-search"/>
-                        <InputText
-                            value={this.state.termoDeBusca}
-                            //largura máxima
-                            className="w-full"
-                            onChange={this.onTermoAlterado}
-                            placeholder={this.props.dica}
-                        />
-                    </span>                
-                    <Button 
-                        label="OK"
-                        className="p-button-outlined mt-2" 
+    //warning sugere desestruturar pois props muda sempre
+    const {onBuscaRealizada} = props
+    useEffect(() => {
+        let timeoutID
+        if (termoDeBusca && termoDeBusca.length >= 3)
+           timeoutID = setTimeout(() => onBuscaRealizada(termoDeBusca), 2000)
+        
+        return () => {
+            clearTimeout(timeoutID)
+        }
+        //warning sugere especificar todas as dependências, mesmo a função. Neste caso, não faz diferença
+    }, [termoDeBusca, onBuscaRealizada])
+    return (
+            <div className="flex flex-column">
+                {/* ícone à esquerda, largura máxima */}
+                <span className="p-input-icon-left w-full">
+                    <i className="pi pi-search"/>
+                    <InputText
+                        value={termoDeBusca}
+                        //largura máxima
+                        onChange={atualizarTermoDeBusca}
+                        className="w-full"
                     />
-                </div>
-            </form>
-        )
-    }
+                </span>                
+            </div>
+    )
 }
+// export default class Busca extends Component {
+//     state = {
+//         termoDeBusca: ''
+//     }
+//     onTermoAlterado = (event) => {
+//         console.log(event.target.value)
+//         this.setState({termoDeBusca: event.target.value})
+//     }
+
+//     onFormSubmit = (event) => {
+//         //não deixa o navegador submeter o form
+//         event.preventDefault()
+//         this.props.onBuscaRealizada(this.state.termoDeBusca)
+
+//     }
+
+//     render() {
+        
+//     }
+// }
+
+export default Busca
 
 Busca.defaultProps ={
     dica: 'Digite algo que deseja ver...'
